e7e75f33db07bc98ad47ccde8a82e9a2cc022336
WuXueqian <wuxueqian2010@hotmail.com>
2017-03-30 12:01:02 +0800

    Add mongo links demo

--- node/index.js
+++ node/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var mongo = require('mongoose')
+
+var db = mongo.connect('mongodb://mongo:27017/dev'); // see docker-compose.yml nodeapp links mongo
 
 app.get('/', function(req, res){
   res.send('hello world');
